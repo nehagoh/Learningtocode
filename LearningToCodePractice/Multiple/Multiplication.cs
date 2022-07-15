4009d0df0b25f7fb943f085cf3dfcf7860996586
nehagoh <gohilnehad313@gmail.com>
2022-07-15 23:49:18 +0530

    Added Multiplication code

---
+++ LearningToCodePractice/Multiple/Multiplication.cs
@@ -0,0 +1,17 @@
+﻿namespace Multiple
+{
+    public class Multiplication
+    {
+        int num1, num2;
+        public void multiply()
+        {
+            int product;
+            Console.WriteLine("Enter first number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            product = (num1 * num2);
+            Console.WriteLine("Product is:{0}",product);
+        }
+    }
+}
