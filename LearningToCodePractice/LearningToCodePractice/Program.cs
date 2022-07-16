a38df222d5d9066b083f0541e3778a15b4d578e6
nehagoh <gohilnehad313@gmail.com>
2022-07-16 22:01:03 +0530

    added user input

--- LearningToCodePractice/LearningToCodePractice/Program.cs
+++ LearningToCodePractice/LearningToCodePractice/Program.cs
@@ -1,6 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Multiple;
-
-Console.WriteLine("Hello, Welcome to coding practice!");
-Multiplication mul = new Multiplication();
-mul.multiply();
+﻿int choice;
+do
+{
+    Console.WriteLine("What type of operation you want to perform ?");
+    Console.WriteLine("1. Calculator");
+    Console.WriteLine("2. String Manipulation");
+    Console.WriteLine("0. EXIT");
+    choice = Convert.ToInt32(Console.ReadLine());
+    switch(choice)
+    {
+        case 1: 
+            Console.WriteLine("Calculator is selected");
+            break;
+        case 2:
+            Console.WriteLine("String Manipulation is selected");
+            break;
+        case 0:
+            Console.WriteLine("Thank you for using this application");
+            break;
+        default:
+            Console.WriteLine("Wrong choice selected");
+            break;
+    }
+} while (choice != 0);
